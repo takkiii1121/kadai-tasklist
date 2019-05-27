03ab71582d492cdbbc15e926b6ec6b414ee16967
mayu.takino <prz465bueu8i6fqbtq2e@docomo.ne.jp>
2019-05-27 01:57:03 +0000

    CRUD tasks

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,58 @@
+class TasksController < ApplicationController
+    def index
+        @tasks = Task.all
+    end
+    
+    def show
+        @task = Task.find(params[:id])
+    end
+    
+    def new
+        @task = Task.new
+    end
+    
+    def create
+        @task = Task.new(task_params)
+        
+        if @task.save
+            flash[:success] = 'Taskが正常に保存されました'
+            redirect_to @task
+        else
+            flash.now[:danger] = 'Taskが保存されませんでした'
+            render :new
+        end
+    end
+    
+    def edit
+        @task = Task.find(params[:id])
+    end
+    
+    def update
+        @task = Task.find(params[:id])
+        
+        if @task.update(task_params)
+            flash[:success] = 'Taskは正常に保存されました'
+            redirect_to @task
+        else
+            flash.now[:danger] = 'Taskは保存されませんでした'
+            render :edit
+        end
+    end
+    
+    def destroy
+        @task = Task.find(params[:id])
+        @task.destroy
+        
+        flash[:success] = 'Taskは正常に削除されました'
+        redirect_to tasks_url
+    end
+    
+end
+
+
+ private
+    
+    def task_params
+        params.require(:task).permit(:content)
+    end
+    
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
+    root to: 'tasks#index'
+    resources :tasks
+    # get 'tasks/:id', to: 'tasks#show'
+    # post 'tasks', to: 'tasks#create'
+    # put 'tasks/id:', to: 'tasks#update'
+    # delete 'tasks/id:', to: 'tasks#destroy'
+    
+    # get 'tasks', to: 'tasks#index'
+    
+    # get 'tasks/new', to: 'tasks#new'
+    
+    # get 'tasks/id:/edit' to: 'tasks#edit'
+    
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
