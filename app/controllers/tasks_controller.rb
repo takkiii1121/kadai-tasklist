999000f9d148e153c563f591aa8af029a3850ab0
mayu.takino <prz465bueu8i6fqbtq2e@docomo.ne.jp>
2019-06-17 11:25:34 +0000

    shuusei

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
     before_action :set_task, only: [:show, :edit, :update, :destroy]
-    before_action :require_user_logged_in, only: [:index, :show, :edit]
+    before_action :require_user_logged_in, only: [:new, :index, :show, :edit]
     before_action :correct_user, only: [:edit, :show, :update, :destroy]
     
     def index
